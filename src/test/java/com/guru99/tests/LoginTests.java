df5f4fd374a30b00bb0ec0797621dd5ca8ec0a66
RajeshMuthusamy <multicloud.rajesh@gmail.com>
2022-07-02 22:33:38 +0100

    Added config.properties , replaced chrome driver, included base tests

--- src/test/java/com/guru99/tests/LoginTests.java
+++ src/test/java/com/guru99/tests/LoginTests.java
@@ -1,4 +1,19 @@
 package com.guru99.tests;
 
-public class LoginTests {
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests extends BaseTests {
+    @Test
+    public void verifyLoginWithCorrectCredentials(String userName, String userPassword) {
+        loginPage.loginToApplication(userName, userPassword);
+        String actualTitle = commonDriver.getTitleOfThePage();
+        String expectedTitle = "Guru99 Bank Manager HomePage";
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void justanotherTest() {
+        System.out.println("This is just a test");
+    }
 }
